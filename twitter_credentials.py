5f88a8f227ea534fa2647b5b1e5ee683360b4adb
Ashish Kumar <ak-org@users.noreply.github.com>
2018-12-09 16:21:03 -0800

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,6 +1,8 @@
+# Insert your keys below. You can obtain these keys from twitter's developer website https://developer.twitter.com
+
 class api_credentials():
     def __init__(self):
-        self.access_token = '629646618-5Mez9IVwO0wuRrrLefyD4t2KNRFbALH5WqSOROuF'
-        self.access_token_secret = 'RfumKYLnnnqJbwzMZLV0o1tHMdslYSZTheXn935oNy57B'
-        self.consumer_key  = 'VskuqmYd8z3bwjqy9EeEq3G1E'
-        self.consumer_secret = 'XL14K5bEl9xEe0Fvssu3yNLPquXeyVPf63I8OscDd27sAFGayr'
+        self.access_token = ' '
+        self.access_token_secret = ' '
+        self.consumer_key  = ' '
+        self.consumer_secret = ' '
